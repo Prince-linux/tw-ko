81fe1e32433579f75b928383b46f05ed37995455
Prince Boamah <regioths@gmail.com>
2021-12-06 12:14:24 +0000

    commented out corsheader middleware

--- backend/p_management/settings.py
+++ backend/p_management/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'corsheader.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'p_management.urls'
