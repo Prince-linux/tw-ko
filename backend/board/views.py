2c10f9eafa4dd060daf47da1fce3db3f90186a58
Prince Boamah <regioths@gmail.com>
2021-12-06 12:13:38 +0000

    Added Boardview to viewsets

--- backend/board/views.py
+++ backend/board/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from rest_framework import serializers, viewsets
+
+from board.models import Board, Card, List,  Attachment
+
+from .serializers import BoardSerializer, CardSerializer, ListSerializer, AttachmentSerializer
+
+
+class BoardView(viewsets.ModelViewSet):
+    serializer_class = BoardSerializer
+    queryset = Board.objects.all()
