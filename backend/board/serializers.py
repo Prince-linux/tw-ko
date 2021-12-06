58c3d1bdc28e2f95f24ef19316a8c4e6dfbceb4a
Prince Boamah <regioths@gmail.com>
2021-12-06 12:11:54 +0000

    Changed listserializer fields to all

--- backend/board/serializers.py
+++ backend/board/serializers.py
@@ -1,7 +1,7 @@
 from django.db.models import fields
 from rest_framework import serializers 
 
-from .models import Board, List
+from .models import Board, Card, List, Attachment
 
 
 class BoardSerializer(serializers.ModelSerializer):
@@ -9,11 +9,21 @@ class BoardSerializer(serializers.ModelSerializer):
         model = Board
         fields = "__all__"
 
+
+
 class ListSerializer(serializers.ModelSerializer):
     class Meta:
         model = List
-        exclude = ['board']
+        fields = "__all__"
 
-    # def get_items(self, obj):
-    #     queryset = Item.objects.filter(list=obj).order_by('order')
-    #     return ItemSerializer(queryset, many=True).data
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = "__all__"
+
+class AttachmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Attachment
+        fields = '__all__'
