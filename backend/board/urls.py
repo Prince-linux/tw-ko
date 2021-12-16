06cd2366b4a7cc72ec271bb85b8015e454b722e8
Prince Boamah <regioths@gmail.com>
2021-12-16 15:56:17 +0000

    Added urls to attachments

--- backend/board/urls.py
+++ backend/board/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path("boards/", views.board_list_create, name="boards"),  # GET, POST
     path('boards/<int:pk>/', views.board_detail, name = 'board-detail'),
+    path('boards/attachment/', views.attachment_create, name='board-attachment'),
+    path('boards/attachment/<int:pk>/', views.attachment_detail, name='board-attachment-detail'),
 ]
