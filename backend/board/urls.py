dbecac3ad79f7ad046b2f1ac84f9e62935686d42
Prince Boamah <regioths@gmail.com>
2021-12-23 05:15:23 +0000

    Edited broken urls

--- backend/board/urls.py
+++ backend/board/urls.py
@@ -5,7 +5,11 @@ from . import views
 
 urlpatterns = [
     path("boards/", views.board_list_create, name="boards"),  # GET, POST
-    path('boards/<int:pk>/', views.board_detail, name = 'board-detail'),
+    path('boards/detail/<int:pk>/', views.board_detail, name = 'board-detail'),
     path('boards/attachment/', views.attachment_create, name='board-attachment'),
     path('boards/attachment/<int:pk>/', views.attachment_detail, name='board-attachment-detail'),
+    path('boards/card/', views.card_create, name='board-card'),
+    path('boards/card/<int:pk>/', views.card_detail, name='board-card-detail'),
+    path("boards/list/", views.list_create, name="list-create"), 
+    path('boards/list/<int:pk>/', views.list_detail, name='board-list-detail'),
 ]
