f9c45a2824ac8bc90f279ed238d8f5119e6d8c9b
Prince Boamah <regioths@gmail.com>
2021-12-23 05:15:45 +0000

    Edited broken views

--- backend/board/views.py
+++ backend/board/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 
-# from rest_framework import serializers, viewsets
+from rest_framework import serializers, viewsets
 from rest_framework.decorators import api_view
 from rest_framework import status 
 from rest_framework.response import Response 
@@ -25,7 +25,9 @@ from .serializers import BoardSerializer, CardSerializer, ListSerializer, Attach
 
   function based views / class based views
 """
-
+class BoardView(viewsets.ModelViewSet):
+    serializer_class = BoardSerializer
+    queryset = Board.objects.all()
 
 @api_view(["GET", "POST"])
 def board_list_create(request):
@@ -92,12 +94,14 @@ def card_create(request):
     :returns:
     """
     if request.method == "POST":
+        # list = List.objects.get(list_pk=list_pk)
+        # attachment = Attachment.objects.get(attachment_pk=attachment_pk)
         serializer = CardSerializer(data=request.data)
         if not serializer.is_valid():
             """
             data = {
-                success: True / False,a
-                errors: errors that occured.
+                success: True / False,
+                errors: errors that occurred.
             }
             """
             return Response(data={"success": False, "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
@@ -111,21 +115,77 @@ def card_create(request):
 
     serializer = CardSerializer(card, many=True)
 
-    return Response(data={"success": True, "boards": serializer.data})
+    return Response(data={"success": True, "cards": serializer.data})
 
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def card_detail(request, pk):
+    try:
+        # attachment = Attachment.objects.get(pk=pk)
+        # list = List.objects.get(pk=pk)
+        card = Card.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+  
+    if request.method == 'GET':
+        # list = List.objects.get(pk=pk)
+        serializer = CardSerializer(card)
+        return Response(serializer.data)
+  
+    elif request.method == 'PUT':
+        serializer = CardSerializer(card, data=request.data)
+  
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+    elif request.method == 'DELETE':
+        card.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(["GET", "POST"])
+def list_create(request):
+    """
+    Gets a list of boards if request.method is GET 
+    or creates a board if request.method is POST
+    :param request:
+    :returns:
+    """
+    if request.method == "POST":
+        serializer = ListSerializer(data=request.data)
+        if not serializer.is_valid():
+            """
+            data = {
+                success: True / False,
+                errors: errors that occurred.
+            }
+            """
+            return Response(data={"success": False, "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+
+        return Response(data={"success": True, "board": serializer.data}, status=status.HTTP_201_CREATED)
+
+
+    list = List.objects.all()
+
+    serializer = ListSerializer(list, many=True)
+
+    return Response(data={"success": True, "boards": serializer.data})
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def list_detail(request, pk):
     try:
-        board = Board.objects.get(pk=pk)
         list = List.objects.get(pk=pk)
-    except Board.DoesNotExist:
+    except List.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
   
     if request.method == 'GET':
         list = List.objects.get(pk=pk)
-        serializer = ListSerializer(list, board)
+        serializer = ListSerializer(list)
         return Response(serializer.data)
   
     elif request.method == 'PUT':
@@ -155,21 +215,21 @@ def attachment_create(request):
             """
             data = {
                 success: True / False,
-                errors: errors that occured.
+                errors: errors that occurred.
             }
             """
             return Response(data={"success": False, "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer.save()
 
-        return Response(data={"success": True, "board": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(data={"success": True, "attachment": serializer.data}, status=status.HTTP_201_CREATED)
 
 
     attachment = Attachment.objects.all()
 
     serializer = AttachmentSerializer(attachment, many=True)
 
-    return Response(data={"success": True, "boards": serializer.data})
+    return Response(data={"success": True})
 
 
 @api_view(['GET', 'DELETE'])
