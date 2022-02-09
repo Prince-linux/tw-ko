7bf61e65eebd8197c8528d89a6de91ef8f053986
Prince Boamah <regioths@gmail.com>
2022-02-09 21:16:43 +0000

    Attachment view Test now passes

--- backend/board/urls.py
+++ backend/board/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 
 urlpatterns = [
-    path("boards/", views.board_list_create, name="boards"),  # GET, POST
-    path('boards/detail/<int:pk>/', views.board_detail, name = 'board-detail'),
-    path('boards/attachment/', views.attachment_create, name='board-attachment'),
-    path('boards/attachment/<int:pk>/', views.attachment_detail, name='board-attachment-detail'),
-    path('boards/card/', views.card_create, name='board-card'),
-    path('boards/card/<int:pk>/', views.card_detail, name='board-card-detail'),
-    path("boards/list/", views.list_create, name="list-create"), 
-    path('boards/list/<int:pk>/', views.list_detail, name='board-list-detail'),
+    path("boards/", views.board_list_create, name="boards"),  # LIST (GET), POST
+    path('boards/<int:pk>/', views.board_detail, name = 'board-detail'), # GET with id
+    path('boards/attachment/', views.attachment_create, name='attachments'),
+    path('boards/attachment/<int:pk>/', views.attachment_detail, name='attachment-detail'),
+    path('boards/card/', views.card_create, name='card'),
+    path('boards/card/<int:pk>/', views.card_detail, name='card-detail'),
+    path("boards/", views.list_create, name="list-create"), 
+    path('boards/list/<int:pk>/', views.list_detail, name='list-detail'),
 ]
