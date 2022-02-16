a7b012001392396781d4ce3daeceb86f5a06c497
Prince Boamah <regioths@gmail.com>
2022-02-16 17:14:42 +0000

    Board and attachment tests passes

--- backend/board/urls.py
+++ backend/board/urls.py
@@ -1,15 +1,18 @@
-from django.urls import path 
+from django.urls import path
 
-from . import views 
+from . import views
 
 
 urlpatterns = [
-    path("boards/", views.board_list_create, name="boards"),  # LIST (GET), POST
-    path('boards/<int:pk>/', views.board_detail, name = 'board-detail'), # GET with id
+    path("boards/", views.board_list_create,
+         name="boards"),  # LIST (GET), POST
+    path('boards/<int:pk>/', views.board_detail,
+         name='board-detail'),  # GET with id
     path('boards/attachment/', views.attachment_create, name='attachments'),
-    path('boards/attachment/<int:pk>/', views.attachment_detail, name='attachment-detail'),
+    path('boards/attachment/<int:pk>/',
+         views.attachment_detail, name='attachment-detail'),
     path('boards/card/', views.card_create, name='card'),
     path('boards/card/<int:pk>/', views.card_detail, name='card-detail'),
-    path("boards/", views.list_create, name="list-create"), 
+    path("boards/list/", views.list_create, name="list"),
     path('boards/list/<int:pk>/', views.list_detail, name='list-detail'),
 ]
